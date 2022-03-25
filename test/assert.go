699621ea252675cdfdef9d93d612286fe85f4c60
Ivo Kubjas <ivo.kubjas@consensys.net>
2022-03-25 15:31:07 +0100

    feat: add test.NoFuzzing() testing option

--- test/assert.go
+++ test/assert.go
@@ -169,11 +169,13 @@ func (assert *Assert) ProverSucceeded(circuit frontend.Circuit, validAssignment 
 		}
 	}
 
-	// TODO may not be the right place, but ensures all our tests call these minimal tests
-	// (like filling a witness with zeroes, or binary values, ...)
-	assert.Run(func(assert *Assert) {
-		assert.Fuzz(circuit, 5, opts...)
-	}, "fuzz")
+	if opt.fuzzing {
+		// TODO may not be the right place, but ensures all our tests call these minimal tests
+		// (like filling a witness with zeroes, or binary values, ...)
+		assert.Run(func(assert *Assert) {
+			assert.Fuzz(circuit, 5, opts...)
+		}, "fuzz")
+	}
 }
 
 // ProverSucceeded fails the test if any of the following step errored:
@@ -456,6 +458,7 @@ func (assert *Assert) options(opts ...TestingOption) testingConfig {
 		witnessSerialization: true,
 		backends:             backend.Implemented(),
 		curves:               ecc.Implemented(),
+		fuzzing:              true,
 	}
 	for _, option := range opts {
 		err := option(&opt)
